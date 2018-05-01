7fb5b0da362b08da8a983904a0ca457b0a13f496
Leo2691 <LevAntonov@yandex.ru>
2018-05-01 16:29:13 +0300

    Deviding upload of data for Windows and for Mac OD

--- Mashine Learning/Week 3/1_SVM.py
+++ Mashine Learning/Week 3/1_SVM.py
@@ -13,30 +13,44 @@ import matplotlib.pyplot as plt
 
 
 def SVMMethod():
-    # загружаем из файла без заголовка header = None
-    X = pd.read_csv("/Users/lev/Образование/Машинное обучение/Week 3/3_work1_support vector machines/svm-data.csv", usecols=[1], header=None)
-    Y = pd.read_csv("/Users/lev/Образование/Машинное обучение/Week 3/3_work1_support vector machines/svm-data.csv", usecols=[0], header=None)
-    print(X, Y)
+    # загружаем из файла без заголовка header = None ДЛЯ MAC OS
+    #X = pd.read_csv("/Users/lev/Образование/Машинное обучение/Week 3/3_work1_support vector machines/svm-data.csv", usecols=[1], header=None)
+    #Y = pd.read_csv("/Users/lev/Образование/Машинное обучение/Week 3/3_work1_support vector machines/svm-data.csv", usecols=[0], header=None)
+
+    # загружаем из файла без заголовка header = None ДЛЯ WINDOWS
+    X = pd.read_csv("D:\svm-data.csv", usecols=[1, 2], header=None)
+    Y = pd.read_csv("D:\svm-data.csv", usecols=[0], header=None)
+    # print(Y, X)
 
     # условия линейно разделимой выборки
     svm = SVC(kernel='linear', C=100000, random_state=241)
 
     svm.fit(X, Y)
 
-    #X_combined_std = np.vstack((X))
-    #y_combined_std = np.vstack((Y))
+    # print(svm.support_vectors_)
+
+
+    # print(1)
+
+    X = pd.DataFrame.as_matrix(X)
+    Y = pd.DataFrame.as_matrix(Y)
+
+    """plot_decision_regions требует, чтобы y был типа int и передавался в формате [0,1, ..., 1]"""
+    Y = Y.astype(int)
+    """flatten возвращает одномерный (сглаженый) массив из формата n x m"""
+    Y = Y.flatten()
+
+    # X = np.ndarray(X)
+    # Y = np.ndarray(Y)
+    # X_combined_std = np.vstack((X))
+    # y_combined_std = np.vstack((Y))
     # test_idx = range(X_combined_std.shape[0], )
 
-    # Plotting decision regions
-    plot_decision_regions(X, Y, clf=svm, legend=1)
-
-    # Adding axes annotations
+    # Рисуем график
+    plot_decision_regions(X, Y, clf=svm, legend=2)
     plt.xlabel('1')
     plt.ylabel('1')
     plt.title('SVM')
     plt.show()
-
-    print(1)
-
 
 SVMMethod()
